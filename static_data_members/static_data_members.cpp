a040743709b2eaa456d0d303c4c259eeb247afe4
jack.chan <jack.chan@robotemi.com>
2022-09-01 14:54:01 +0800

    静态成员变量的理解

---
+++ static_data_members/static_data_members.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+//reference to https://www.geeksforgeeks.org/static-data-members-c/?ref=gcse
+class A {
+  int x;
+
+ public:
+  A() { cout << "A's constructor called " << endl; }
+};
+
+class B {
+  static A a;//这个地方是静态成员变量的声明
+
+ public:
+  B() { cout << "B's constructor called " << endl; }
+  static A getA() { return a; }
+};
+A B::a;//个人理解,这个地方是定义,也就是初始化
+int main() {
+  B b;
+  A a = b.getA();
+  return 0;
+}
