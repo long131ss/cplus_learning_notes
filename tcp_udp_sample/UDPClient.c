20a3e281b448efb221e11d7413cd71977da079b5
jack.chan <jack.chan@robotemi.com>
2022-09-30 10:52:57 +0800

    对UDP协议的理解

--- tcp_udp_sample/UDPClient.c
+++ tcp_udp_sample/UDPClient.c
@@ -1,4 +1,11 @@
 // Client side implementation of UDP client-server model
+// UDP Client :  
+
+// 1.Create a UDP socket.
+// 2.Send a message to the server.
+// 3.Wait until response from the server is received.
+// 4.Process reply and go back to step 2, if necessary.
+// 5.Close socket descriptor and exit.
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -27,7 +34,8 @@ int main() {
 	memset(&servaddr, 0, sizeof(servaddr));
 		
 	// Filling server information
-	servaddr.sin_family = AF_INET;
+	//The address family for the transport address. This member should always be set to AF_INET.
+	servaddr.sin_family = AF_INET;//sin means "socket internet"
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 		
@@ -40,10 +48,11 @@ int main() {
 			
 	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
 				MSG_WAITALL, (struct sockaddr *) &servaddr,
-				&len);
+				&len);//servaddr 也是可以从header中获取的
 	buffer[n] = '\0';
 	printf("Server : %s\n", buffer);
 	
 	close(sockfd);
 	return 0;
 }
+
--- tcp_udp_sample/UDPServer.c
+++ tcp_udp_sample/UDPServer.c
@@ -1,4 +1,19 @@
 // Server side implementation of UDP client-server model
+
+// UDP Server :  
+// 1.Create a UDP socket.
+// 2.Bind the socket to the server address.
+// 3.Wait until the datagram packet arrives from the client.
+// 4.Process the datagram packet and send a reply to the client.
+// 5.Go back to Step 3.
+
+
+//为什么server端需要指定一个端口号呢? 因为client客户端需要在已知的端口号情况下,和server端通信
+//而server端在业务设计上是是负责监听来自client客户端的消息,消息里面包含客户端socket的端口号,一般而言,
+//这个端口号是由系统自动分配
+//然后基于端口号,发送消息给客户端
+//https://stackoverflow.com/questions/4118241/what-client-side-situations-need-bind
+//What client-side situations need bind()?
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -24,7 +39,7 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 		
-	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&servaddr, 0, sizeof(servaddr)); //初始化结构体分配内存空间
 	memset(&cliaddr, 0, sizeof(cliaddr));
 		
 	// Filling server information
@@ -46,11 +61,11 @@ int main() {
 	
 	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
 				MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-				&len);
+				&len); //cliaddr从UDP header中获取,个人理解一般来说server跟client是1对多的关系
 	buffer[n] = '\0';
 	printf("Client : %s\n", buffer);
 	sendto(sockfd, (const char *)hello, strlen(hello),
-		MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+		MSG_CONFIRM, (const struct sockaddr *) &cliaddr,//
 			len);
 	printf("Hello message sent.\n");
 	printf("sockaddr %s",((const struct sockaddr *)&cliaddr)->sa_data);
